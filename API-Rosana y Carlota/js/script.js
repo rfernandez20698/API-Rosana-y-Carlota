6be1976230e8aab4c8ef55c0cbab62241e229b6e
rfernandez20698 <rfernandez20698@iesjoanramis.org>
2024-12-18 11:58:07 +0100

    Add files via upload

--- API-Rosana y Carlota/js/script.js
+++ API-Rosana y Carlota/js/script.js
@@ -1,36 +1,20 @@
-const data = null;
+var name = document.getElementById("n1");
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/city?name=' + name,
+    headers: { 'X-Api-Key': '0sYi5iZnSZ75r99W8IcI0Q==CKN88fC7nDTLS6rg'},
+    contentType: 'application/json',
+    success: function(result) {
 
-var name = 'San Francisco'
+        console.log(result)
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    }
 
 
-
-xhr.addEventListener('readystatechange', function () {
-	if (this.readyState === this.DONE) {
-        let resultado = JSON.parse(this.response)
-    
-        $.ajax({
-            method: 'GET',
-            url: 'https://api.api-ninjas.com/v1/city?name=' + name,
-            headers: { 'X-Api-Key': '0sYi5iZnSZ75r99W8IcI0Q==CKN88fC7nDTLS6rg'},
-            contentType: 'application/json',
-            success: function(result) {
-                console.log(result);
-            },
-            error: function ajaxError(jqXHR) {
-                console.error('Error: ', jqXHR.responseText);
-            }
-
-        });
-        console.log(resultado)
-	}
 });
-
-
-
-
 
 
 
