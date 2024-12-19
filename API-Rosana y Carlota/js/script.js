ceba8fc04df0f52638933b07eacc9472c1758eab
rfernandez20698 <rfernandez20698@iesjoanramis.org>
2024-12-19 08:31:05 +0100

    Add files via upload

--- API-Rosana y Carlota/js/script.js
+++ API-Rosana y Carlota/js/script.js
@@ -1,4 +1,4 @@
-var name = document.getElementById("n1");
+let name = document.getElementById("name").value;
 
 $.ajax({
     method: 'GET',
@@ -6,15 +6,17 @@ $.ajax({
     headers: { 'X-Api-Key': '0sYi5iZnSZ75r99W8IcI0Q==CKN88fC7nDTLS6rg'},
     contentType: 'application/json',
     success: function(result) {
-
-        console.log(result)
+        console.log(result);
     },
     error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
+        console.error('Error:', jqXHR.responseText);
     }
-
-
 });
+
+document.getElementById("añadir").addEventListener('click', function(){
+
+    
+})
 
 
 
